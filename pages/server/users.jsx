fc55bc66ba17ccba9b339a55a575e5b11017fe39
techshalinii <shalinikumari9142@gmail.com>
2024-10-13 23:25:34 +0530

    ssr used

---
+++ pages/server/users.jsx
@@ -0,0 +1,19 @@
+const UserPage = (props) => {
+    console.log('props', props)
+    return(
+        <div>
+            <h1>Users (ssr)</h1>{
+                props.data.users.map(user => <li key={user.id}>{user.firstName}</li>)
+            }
+        </div>
+    )
+}
+export const getServerSideProps = async() => {
+    const data = await(await fetch('https://dummyjson.com/users')).json()
+    return{
+        props: {
+           data,
+        }
+    }
+};
+export default UserPage;
