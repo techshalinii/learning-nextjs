9c5bac02592c4505d61cac581f25c3642f301b7c
techshalinii <shalinikumari9142@gmail.com>
2024-10-13 23:27:19 +0530

    ssg used

---
+++ pages/static/users/index.jsx
@@ -0,0 +1,21 @@
+const UsersPage = (props) => {
+    return(
+        <div>
+            <h1>
+                Users Page(SSG)
+            </h1>
+            {
+                props.data.users.map((user) => (<li key={user.id}>{user.firstName}</li>))
+            }
+        </div>
+    )
+}
+export const getStaticProps = async() => {
+    const data = await(await fetch('https://dummyjson.com/users')).json()
+    return{
+        props: {
+           data,
+        }
+    }
+}
+export default UsersPage;
